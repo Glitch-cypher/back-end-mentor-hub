4569ea6d9d727a3136cdda67102d825acd4d94c8
Dan <up765064@myport.ac.uk>
2020-11-11 13:45:27 +0000

    url

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+//http://localhost:5000/feedback
 app.use("/", indexRouter);
 
 module.exports = app;
